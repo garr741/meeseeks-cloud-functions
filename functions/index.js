7d730494b2e7dcdafefeaf4caaafcfe0884d53d2
Tylor Garrett <tylorgarrett@gmail.com>
2017-07-11 01:06:15 -0400

    added stuff

--- functions/index.js
+++ functions/index.js
@@ -1,73 +1,49 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const request = require('request')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+admin.initializeApp(functions.config().firebase)
 
 exports.actions = functions.https.onRequest((request, response) => {
-  console.log(request.body)
   response.status(200).end()
-})
-
-exports.headcount = functions.https.onRequest((request, response) => {
-  response.status(200).end()
-  var reqBody = request.body
-  var responseURL = reqBody.response_url
-  var eventName = reqBody.text
-  if (reqBody.token == 1){
-    response.status(403).end("Access forbidden")
-  } else {
-    var message = {
-      "text": "Hey! The " + eventName + " is coming up soon!",
-      "attachments": [
-        {
-          "text": "Will you be able to attend?",
-          "fallback": "Something has gone wrong, ignore this message!",
-          "callback_id": "button_response",
-          "color": "#3AA3E3",
-          "attachment_type": "default",
-          "actions": [
-            {
-              "name": "Yes",
-              "text": "Yes",
-              "type": "button",
-              "value": "Yes",
-              "style": "primary",
-            },
-            {
-              "name": "No",
-              "text": "No",
-              "type": "button",
-              "value": "No",
-              "style": "danger"
-            },
-            {
-              "name": "Maybe",
-              "text": "Maybe",
-              "type": "button",
-              "value": "Maybe"
-            }
-          ]
-        }
-      ]
-    }
-    sendMessageToSlackResponseURL(responseURL, message)
+  let payload = JSON.parse(request.body.payload)
+  if (payload.callback_id.includes("knockknock") ) {
+    knockKnock(payload)
   }
 })
 
-function sendMessageToSlackResponseURL(responseURL, JSONmessage) {
-  var postOptions = {
-    uri: responseURL,
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    json: JSONmessage
-  }
-  request(postOptions, (error, response, body) => {
-    if (error){
-      console.log(error)
-    }
+function knockKnock(payload) {
+  let responseUrl = payload.response_url
+  let answer = payload.actions[0].name
+  let reply = "You replied " + answer + ". Thanks!"
+  let message = {'text': reply}
+  let user = payload.user
+  let eventName = payload.callback_id.split('~')[1]
+  documentResponse(user, answer, eventName)
+  sendMessageToSlackResponseURL(responseUrl, message)
+}
+
+function documentResponse(user, answer, eventName) {
+  admin.database().ref('/users').orderByChild("name").equalTo(user.name).once('value').then(function(snapshot){
+    let userId = user.id
+    let real_name = snapshot.val()[userId].profile.real_name;
+    admin.database().ref('/events').child(eventName).child(answer).push(real_name)
   })
+  console.log(user, answer)
+}
+
+function sendMessageToSlackResponseURL(responseURL, JSONmessage){
+    let postOptions = {
+        uri: responseURL,
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        json: JSONmessage
+    }
+    request(postOptions, (error, response, body) => {
+        if (error){
+            console.log("Error: " + error)
+        }
+    })
 }
